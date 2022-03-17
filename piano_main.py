b718604b9ae8da83e6742058033d96a6049e14cf
GrassyAirplane <euanlimcs@gmail.com>
2022-03-17 14:54:05 +0800

    Base code

--- piano_main.py
+++ piano_main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+def play_sound(key):
+    #prints key-press
+
+    #plays sound
+    pass
+
+def play_piano():
+    pass
+
+keys = {1: play_sound, 2: play_sound, 3: play_sound, 
+        4: play_sound, 5: play_sound, 6: play_sound, 
+        7: play_sound, 8: play_sound, 9: play_sound, 
+        0: play_sound, '-': play_sound, '=': play_sound}
+
